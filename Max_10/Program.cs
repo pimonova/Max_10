793941ae6c36f901f299df1aa7a0baa605608087
Антонина Пимонова <ae.pimonova@gmail.com>
2022-10-17 16:14:09 +0300

    Добавьте файлы проекта.

---
+++ Max_10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Max_10 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(nums.Max<int>());
+        }
+    }
+}
